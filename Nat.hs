d50c94bc9e9457a5052121b99053f58400460b9f
Ordep-42 <p.galvao@outlook.com.br>
2023-09-18 19:01:36 -0300

    Adição, multiplicação e exponênciação

--- Nat.hs
+++ Nat.hs
@@ -1,4 +1,22 @@
 module Nat where
 
+import Prelude hiding 
+    ((+), (*), (^), fact, fib, double, min, max)
+
 data Nat = O | S Nat
     deriving (Eq, Show)
+
+(+) :: Nat -> Nat -> Nat
+(+) n O = n
+(+) n (S m) = S (n + m)
+
+(*) :: Nat -> Nat -> Nat
+(*) n O = O
+(*) n (S m) = n + (n * m)
+
+(^) :: Nat -> Nat -> Nat
+(^) n O = S O
+(^) n (S m) = n * (n ^ m)
+
+
+
