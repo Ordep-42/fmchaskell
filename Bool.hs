2832f98121aa622a6b113477391603708dcdc761
Ordep-42 <p.galvao@outlook.com.br>
2023-10-19 14:33:35 -0300

    Definição do if_then_else para tipo alfa

--- Bool.hs
+++ Bool.hs
@@ -5,6 +5,6 @@ import Prelude hiding (True, False, Bool)
 data Bool = True | False
     deriving (Eq, Show)
 
-if_then_else :: Bool -> Nat -> Nat -> Nat
+if_then_else :: Bool -> a -> a -> a
 if_then_else True n _ = n
 if_then_else False _ m = m
